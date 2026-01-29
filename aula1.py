9e4cba46ccacfc60e2ed05daa3c9eea5845e0e85
Tiago Ribeiro <tg.frnds@outlook.com>
2026-01-29 18:51:18 -0300

    Comentários (aula 1)

--- aula1.py
+++ aula1.py
@@ -1,8 +1,16 @@
+# import pandas as pd
 from pandas import read_csv
 
-# Kaggle - base de dados para devs
-# dataframe
+# Kaggle - base de dados para estudos!
+# https://kaggle.com
+
+# df = DataFrame
 df = read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
+# head() - busca os primeiros 5 dados - head(n) retorna n dados
+# info() - retorna os dados principais da base (colunas, tipos, quantidade)
+# describe() - retorna dados de análise
+# describe(include|exclude) traz mais ou menos dados de análise
 
 print(df.head())
 print(df.info())
@@ -10,6 +18,7 @@ print(df.describe())
 print(df.shape) # tupla (rows, cols)
 rows, cols = df.shape # já associa posição = váriavel
 
+# dict - 'chave': 'valor'
 colunas_ptbr = {
     'work_year': 'ano',
     'experience_level': 'senioridade',
@@ -24,9 +33,12 @@ colunas_ptbr = {
     'company_size': 'tamanho_empresa'
 }
 
+# rename - troca o nome de algum campo (coluna, index etc)
 df.rename(columns=colunas_ptbr, inplace=True)
-print(df.head()) # ou df.columns só para as colunas
+print(df.head()) 
+# df.columns só para as colunas
 
+# value_counts() - contabiliza cada valor de uma determinada coluna
 print(df["senioridade"].value_counts())
 
 senioridade = {
@@ -36,6 +48,7 @@ senioridade = {
     'EX': 'executivo'
 }
 
+# replace() - troca valor por novo valor (ex. 1: sim, 0: não)
 df['senioridade'] = df['senioridade'].replace(senioridade)
 print(df["senioridade"].value_counts())
 
